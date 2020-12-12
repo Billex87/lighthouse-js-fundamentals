0aa7019e4d5b47e5eb2c60cf373d8aac989a06af
Billex87 <billy_mckinnon@hotmail.com>
2020-12-12 20:32:57 +0000

    odd

---
+++ odd.js
@@ -0,0 +1,8 @@
+function isOdd(num) { return num % 2;}
+console.log("1 is " + isOdd(1));
+console.log("2 is " + isOdd(2));
+console.log("3 is " + isOdd(3));
+console.log("4 is " + isOdd(4));
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
